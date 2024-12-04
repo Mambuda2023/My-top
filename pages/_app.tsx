b8d3669edc034825ae6f2f412796cd4c52480a23
Mazmanyan Yura <mazmanyan2018@list.ru>
2024-12-04 17:20:08 +0300

    Настройка проекта

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import "@/styles/globals.css";
+import "@/styles/reset.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
