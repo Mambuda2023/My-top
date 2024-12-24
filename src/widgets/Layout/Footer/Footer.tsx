64b3651d9cb3d88dd99aa30bcf31839084aac98d
Mazmanyan Yura <mazmanyan2018@list.ru>
2024-12-24 20:38:58 +0300

    Адаптив Layout и Footer

--- src/widgets/Layout/Footer/Footer.tsx
+++ src/widgets/Layout/Footer/Footer.tsx
@@ -1,5 +1,27 @@
+import cn from "classnames";
 import { FooterProps } from "./Footer.props";
+import { format } from "date-fns";
+import classNames from "classnames";
+import style from "./Footer.module.css";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <footer {...props}>Footer</footer>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, style.footer)} {...props}>
+      <div>OwlTop © 2020-{format(new Date(), "yyyy")} Все права защищены</div>
+      <nav>
+        <ul className={style.list}>
+          <li className={style.item}>
+            <a href="#" target="_blank">
+              Пользовательское
+            </a>
+          </li>
+          <li className={style.item}>
+            <a href="#" target="_blank">
+              Политика конфиденциальности
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
 };
